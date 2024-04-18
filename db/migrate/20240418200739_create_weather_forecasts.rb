b02b30b8dcd5dba51974433a0d570bc98aca98ad
Alex Slepak <alex.slepak@lovevery.com>
2024-04-18 13:15:06 -0700

    db schema

---
+++ db/migrate/20240418200739_create_weather_forecasts.rb
@@ -0,0 +1,12 @@
+class CreateWeatherForecasts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :weather_forecasts do |t|
+      t.string :zipcode, null: false
+      t.boolean :cached, null: false, default: false
+      t.json :forecast_data, null: false
+      t.json :upcoming_forecast_data
+
+      t.timestamps
+    end
+  end
+end
